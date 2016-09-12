584a9a91e2ecee3f9efa434c002fff3c2643d0d6
Alex <alexellis2@gmail.com>
2016-09-12 20:50:44 +0100

    Add health

--- mkguid_tester/app.js
+++ mkguid_tester/app.js
@@ -5,11 +5,22 @@ const uuid = require('uuid');
 const os = require('os');
 
 let app = express();
-
 let hostname = os.hostname();
 
-app.get("/guid", (req, res) => {  
+let state = {
+    generateFailure: false
+};
+
+app.get("/guid", (req, res) => {
+    if(state.generateFailure) {
+        return res.status(500).end();
+    }
     res.json({ "guid": uuid.v4(), "container": hostname });
+});
+
+app.post("/toggle.failure", (req, res) => {
+    state.generateFailure = !state.generateFailure;  
+    res.status(200).end();
 });
 
 app.listen(9000, () => {  
